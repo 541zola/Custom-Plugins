06bbf84c1153b758a5795e3e237ba8f70684cfab
Nalika31 <97948891+Nalika31@users.noreply.github.com>
2022-01-23 06:11:31 +0700

    Update anu.py

--- plugins/anu.py
+++ plugins/anu.py
@@ -1,26 +1,26 @@
-"Checker Dapa"
+"Shadown"
 import time
 from pyrogram.errors.exceptions.bad_request_400 import YouBlockedUser
 from userge import userge, Message
 from userge.utils.exceptions import StopConversation
 
 
-#chk
-@userge.on_cmd("chk", about={
-	'header': "Auth Only",
-	'usage': "{tr}chk [Input CC]\n"})
+#CC Check Charge 0.1$
+@userge.on_cmd("cd", about={
+	'header': "CC Check Charge 0.1$",
+	'usage': "{tr}cd [Input CC]\n"})
 	
 async def gen(message: Message):
-	"""Cek Auth"""
+	"""CC Check Charge 0.1$"""
 	replied = message.input_str
-	chat = "@rkhackingbot"
-	await message.edit("```Tunggu mas ```")
+	chat = "@Hcsacc_bot"
+	await message.edit("```Wait for result... ```")
 	msgs = []
-	ERROR_MSG = "Ijin dulu sama @Dappzx"
+	ERROR_MSG = "Error"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("/chk {}".format(replied))
+				await conv.send_message("/cd {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -93,22 +93,22 @@ async def gen(message: Message):
 		await message.edit(f"`Bot tidak merespon!`", del_in=5)
 
 
-#bin
-@userge.on_cmd("bin", about={
-	'header': "BIN",
-	'usage': "{tr}bin [Enter bin]\n"})
+#CC Charge $1
+@userge.on_cmd("cg", about={
+	'header': "CC Charge $1",
+	'usage': "{tr}cg [Input CC]\n"})
 	
 async def bin(message: Message):
-	"""BIN Ingfo"""
+	"""CC Charge $1"""
 	replied = message.input_str
-	chat = "@rkhackingbot"
-	await message.edit("```Tunggu mas```")
+	chat = "@Hcsacc_bot"
+	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Ijin dulu sama @Dappzx"
+	ERROR_MSG = "Error"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("/bin {}".format(replied))
+				await conv.send_message("/cg {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
@@ -181,22 +181,110 @@ async def bin(message: Message):
 		await message.edit(f"`Bot tidak merespon!`", del_in=5)
 
 
-#sk
-@userge.on_cmd("sk", about={
-	'header': "Cek SK",
-	'usage': "{tr}sk [Input SK]\n"})
+#Spotify
+@userge.on_cmd("spo", about={
+	'header': "Auto Create Spotify Account",
+	'usage': "{tr}spo [Your Name]\n"})
 	
 async def key(message: Message):
-	"""SK Checker"""
+	"""Auto Create Spotify Account"""
 	replied = message.input_str
-	chat = "@rkhackingbot"
-	await message.edit("```Tunggu mas```")
+	chat = "@Hcsacc_bot"
+	await message.edit("```Wait for result...```")
 	msgs = []
-	ERROR_MSG = "Ijin dulu sama @Dappzx"
+	ERROR_MSG = "Error"
 	try:
 		async with userge.conversation(chat) as conv:
 			try:
-				await conv.send_message("/sk {}".format(replied))
+				await conv.send_message("/spo {}".format(replied))
+			except YouBlockedUser:
+				await message.err(f"**{ERROR_MSG}**", del_in=5)
+				return 
+			msgs = await conv.get_response(timeout=30, mark_read=True)
+			if "Wait" in msgs.text:
+				time.sleep(1)
+				msgs = await conv.get_response(timeout=30, mark_read=True) 
+				if "Wait" in msgs.text:
+					time.sleep(1)
+					msgs = await conv.get_response(timeout=30, mark_read=True) 
+					if "Wait" in msgs.text:
+						time.sleep(1)
+						msgs = await conv.get_response(timeout=30, mark_read=True) 
+						if "Wait" in msgs.text:
+							time.sleep(1)
+							msgs = await conv.get_response(timeout=30, mark_read=True)
+							if "Wait" in msgs.text:
+								time.sleep(1)
+								msgs = await conv.get_response(timeout=30, mark_read=True)
+								if "Wait" in msgs.text:
+									time.sleep(1)
+									msgs = await conv.get_response(timeout=30, mark_read=True)
+									if "Wait" in msgs.text:
+										time.sleep(1)
+										msgs = await conv.get_response(timeout=30, mark_read=True)
+										if "Wait" in msgs.text:
+											time.sleep(1)
+											msgs = await conv.get_response(timeout=30, mark_read=True)
+											if "Wait" in msgs.text:
+												time.sleep(1)
+												msgs = await conv.get_response(timeout=30, mark_read=True)
+												if "Wait" in msgs.text:
+													time.sleep(1)
+													msgs = await conv.get_response(timeout=30, mark_read=True)
+													if "Wait" in msgs.text:
+														time.sleep(1)
+														msgs = await conv.get_response(timeout=30, mark_read=True) 
+														if "Wait" in msgs.text:
+															time.sleep(1)
+															msgs = await conv.get_response(timeout=30, mark_read=True)  
+															if "Wait" in msgs.text:
+																time.sleep(1)
+																msgs = await conv.get_response(timeout=30, mark_read=True)  
+																if "Wait" in msgs.text:
+																	time.sleep(1)
+																	msgs = await conv.get_response(timeout=30, mark_read=True) 
+																	if "Wait" in msgs.text:
+																		time.sleep(1)
+																		msgs = await conv.get_response(timeout=30, mark_read=True) 
+																		if "Wait" in msgs.text:
+																			time.sleep(1)
+																			msgs = await conv.get_response(timeout=30, mark_read=True) 
+																			if "Wait" in msgs.text:
+																				time.sleep(1)
+																				msgs = await conv.get_response(timeout=30, mark_read=True) 
+																				if "Wait" in msgs.text:
+																					time.sleep(1)
+																					msgs = await conv.get_response(timeout=30, mark_read=True) 
+																					if "Wait" in msgs.text:
+																						time.sleep(1)
+																						msgs = await conv.get_response(timeout=30, mark_read=True) 
+																						if "Wait" in msgs.text:
+																							time.sleep(1)
+																							msgs = await conv.get_response(timeout=30, mark_read=True) 
+	except StopConversation:
+		pass 
+	try:
+		await message.edit(f"{msgs.text}")
+	except AttributeError:
+		await message.edit(f"`Bot tidak merespon!`", del_in=5)
+
+
+#3DS
+@userge.on_cmd("3d", about={
+	'header': "3DS Check",
+	'usage': "{tr}3d [Enter full card in format]\n"})
+	
+async def vbv(message: Message):
+	"""3DS Check"""
+	replied = message.input_str
+	chat = "@Hcsacc_bot"
+	await message.edit("```Wait for result...```")
+	msgs = []
+	ERROR_MSG = "Error"
+	try:
+		async with userge.conversation(chat) as conv:
+			try:
+				await conv.send_message("/3d {}".format(replied))
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
