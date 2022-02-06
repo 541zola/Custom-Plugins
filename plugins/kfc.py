2edca7395c3c1e4aa041299350b85673adddb306
Nalika31 <97948891+Nalika31@users.noreply.github.com>
2022-02-07 01:01:19 +0700

    Update kfc.py

--- plugins/kfc.py
+++ plugins/kfc.py
@@ -24,7 +24,19 @@ async def gen(message: Message):
 			except YouBlockedUser:
 				await message.err(f"**{ERROR_MSG}**", del_in=5)
 				return 
-			msgs = await conv.get_response(timeout=40, mark_read=True)
+			msgs = await conv.get_response(timeout=30, mark_read=True)
+			if "Gateway" in msgs.text:
+				time.sleep(1)
+				msgs = await conv.get_response(timeout=2, mark_read=True) 
+				if "Gateway" in msgs.text:
+					time.sleep(1)
+					msgs = await conv.get_response(timeout=2, mark_read=True) 
+					if "Gateway" in msgs.text:
+						time.sleep(1)
+						msgs = await conv.get_response(timeout=2, mark_read=True) 
+						if "Gateway" in msgs.text:
+							time.sleep(1)
+							msgs = await conv.get_response(timeout=30, mark_read=True)
 	except StopConversation:
 		pass 
 	try:
